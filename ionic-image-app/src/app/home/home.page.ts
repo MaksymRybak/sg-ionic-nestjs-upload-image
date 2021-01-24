7da5c8dfcbf6c0c0bfe2c728a5c3fc85d045050f
Maksym Rybak <rybak.maksym@gmail.com>
2021-01-24 16:11:40 +0100

    ionic app: added load and delete images functionalities

--- ionic-image-app/src/app/home/home.page.ts
+++ ionic-image-app/src/app/home/home.page.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ActionSheetController, Platform } from "@ionic/angular";
+import { ApiImage, ApiService } from "../services/api.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  images: ApiImage[] = [];
 
-  constructor() {}
+  constructor(
+    private api: ApiService,
+    private plt: Platform,
+    private actionSheetCtrl: ActionSheetController
+  ) {
+    this.loadImages();
+  }
 
+  loadImages() {
+    this.api.getImages().subscribe((images) => {
+      console.log('images: ', images)
+      this.images = images;
+    });
+  }
+
+  deleteImage(image: ApiImage, index) {
+    this.api.deleteImage(image._id).subscribe(() => {
+      this.images.splice(index, 1);
+    })
+  }
 }
--- ionic-image-app/src/app/services/api.service.ts
+++ ionic-image-app/src/app/services/api.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+export interface ApiImage {
+  _id: string;
+  name: string;
+  createdAt: Date;
+  url: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ApiService {
+  url = "http://localhost:3000";
+  // http://de7abc5ab184.ngrok.io/image/
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  // NOTA: it's different how we upload image from the web and from device!!
+
+  uploadImage() {}
+
+  uploadImageFile() {}
+
+  getImages() {
+    return this.http.get<ApiImage[]>(`${this.url}/image`);
+  }
+
+  deleteImage(id) {
+    return this.http.delete(`${this.url}/image/${id}`);
+  }
+
+
 }
